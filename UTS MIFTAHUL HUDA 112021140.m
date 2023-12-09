7c8223fc2e7c2f3294c61bce82eed744b8237321
mfhlhuda20 <148615673+mfhlhuda20@users.noreply.github.com>
2023-12-09 09:16:27 +0700

    Add files via upload

---
+++ UTS MIFTAHUL HUDA 112021140.m
@@ -0,0 +1,33 @@
+%SOAL 1
+%NAMA : MIFTAHUL HUDA
+%KELAS: A
+
+syms y
+%a.Y" + 5Y + 6Y = 0
+y=dsolve ( ' D2y= -5* DY-6*y')
+%b. Y"- 8Y +16Y =2t=(140)
+y=dsolve ( ' D2y= -8*DY-16*y12+140')
+%c.Y"+y'-2y=4t+(140)
+y=dsolve (' D2y=-1*dy+2*y+4*t+140')
+%d.Y"+2y'-3y=sin2,140t
+y=dsolve ('D2==2*Dy+3*y+sin(2*010*t')
+
+SOAL2
+%A. DENGAN MENGGUNAKAN MATLAB,PLOT FUNGSI PADA PERSAMAAN BERIKUT:
+X= sin (2,140t)
+Y= sin (2,140t)
+Z= 4t
+untuk 0<t <20pi
+%rentang waktu 
+t = linscape (0,20*pi,1000):
+
+%fungsi X = sin (2.100*t)
+X = sin (2.100* t);
+
+%fungsi Y = sin (2.100t)
+Y = sin (2.100*t);
+
+
+
+
+
